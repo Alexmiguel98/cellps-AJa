747627e829971108defaae7740abe946633b44f4
alex jimenez <ajdmor18@gmail.com>
2023-11-11 21:09:40 -0600

    first commit

---
+++ src/forma/JoptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forma;
+
+/**
+ *
+ * @author alex9
+ */
+class JoptionPane {
+
+    static void showMessageDialog(Object object, String usuario_o_contraseña_es_incorrecto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/forma/usuario.java
@@ -0,0 +1,66 @@
+
+package forma;
+
+public class usuario {
+    private int id;
+    private String clave;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+  
+    private String correo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+   
+}
+
+    
+    
+
